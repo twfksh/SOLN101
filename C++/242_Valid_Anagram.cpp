7048192caca6047ec841f8d817b7977915417eaa
Toufiq Shishir <toufiq_a24@outlook.com>
2023-06-11 23:43:20 +0600

    242_Valid_Anagram

---
+++ C++/242_Valid_Anagram.cpp
@@ -0,0 +1,30 @@
+/*
+    Given two strings s and t, return true if t is an anagram of s, 
+    and false otherwise.
+
+    An Anagram is a word or phrase formed by rearranging the letters of a 
+    different word or phrase, typically using all the original letters exactly once.
+
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        int arr[26] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
